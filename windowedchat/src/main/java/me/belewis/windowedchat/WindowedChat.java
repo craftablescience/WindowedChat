18ec1c642bae72801eb18c8cccbb47bad245a602
Brendan Lewis <brendanelewis@gmail.com>
2021-06-07 21:55:19 -0400

    Feat: everything compiles at once! ❤ gradle my beloved

--- windowedchat/src/main/java/me/belewis/windowedchat/WindowedChat.java
+++ windowedchat/src/main/java/me/belewis/windowedchat/WindowedChat.java
@@ -6,6 +6,11 @@ import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Mod(WindowedChat.MODID)
 public class WindowedChat {
     public static final String MODID = "windowedchat";
@@ -15,5 +20,23 @@ public class WindowedChat {
 
     public WindowedChat() {
         MinecraftForge.EVENT_BUS.register(WindowedChatEvents.class);
+        log("Copying client to external directory...");
+
+        Path dir = Paths.get("/windowedchat");
+        if (!Files.exists(dir)) {
+            try {
+                Files.createDirectory(dir);
+            } catch (IOException e) {
+                logError(e.getMessage());
+            }
+        }
+    }
+
+    public static void log(String msg) {
+        LOGGER.info("[" + NAME + "] " + msg);
+    }
+
+    public static void logError(String msg) {
+        log("[" + NAME + "] Error: " + msg + "\n\nTell a developer at https://discord.com/invite/ASgHFkX");
     }
 }
