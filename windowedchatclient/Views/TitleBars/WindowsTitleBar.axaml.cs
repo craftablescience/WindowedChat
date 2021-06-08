c61860ec5fb507454bd44c076370e689d74ebf74
Brendan Lewis <brendanelewis@gmail.com>
2021-06-07 20:25:01 -0400

    Fix(client): clean up csproj

--- windowedchatclient/Views/TitleBars/WindowsTitleBar.axaml.cs
+++ windowedchatclient/Views/TitleBars/WindowsTitleBar.axaml.cs
@@ -10,15 +10,15 @@ namespace windowedchatclient.Views.TitleBars
         {
             InitializeComponent();
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
-            {
-                IsVisible = false;
-            }
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) IsVisible = false;
             else
             {
                 Button minimizeButton = this.FindControl<Button>("MinimizeButton");
                 minimizeButton.Click += MinimizeWindow!;
                 
+                Button settingsButton = this.FindControl<Button>("SettingsButton");
+                settingsButton.Click += OpenSettings!;
+
 #if DEBUG
                 Image windowIcon = this.FindControl<Image>("WindowIcon");
                 windowIcon.DoubleTapped += CloseWindow!;
@@ -36,6 +36,11 @@ namespace windowedchatclient.Views.TitleBars
         {
             Window hostWindow = (Window) VisualRoot;
             hostWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void OpenSettings(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            // todo: add settings popup
         }
 
         private void InitializeComponent()
